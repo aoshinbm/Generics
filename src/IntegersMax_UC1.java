538de8d4b382927d952a4a219ed63acf7a946b83
aoshinbm <aoshinbm28797@gmail.com>
2022-10-07 19:00:50 +0530

    find max integer commit

--- src/IntegersMax_UC1.java
+++ src/IntegersMax_UC1.java
@@ -4,13 +4,11 @@ public class IntegersMax_UC1 <T> {
     T num1;
     T num2;
     T num3;
-
     private IntegersMax_UC1(T num1, T num2, T num3) {
         this.num1 = num1;
         this.num2 = num2;
         this.num3 = num3;
     }
-
     static <T extends Comparable<T>> void maxNumber(T num1, T num2, T num3) {
 
         if (num1.compareTo(num2) >= 0 && num1.compareTo(num3) >= 0) {
@@ -31,6 +29,7 @@ public class IntegersMax_UC1 <T> {
         int n2=sc.nextInt();
         System.out.println("Enter 3rd number:");
         int n3=sc.nextInt();
+
         //find max
         maxNumber(n1,n2,n3);
 
